842eafdebc94dbad78bc3e84a1705a791cb8aaf8
lijunxx <1273587855@qq.com>
2019-11-05 19:24:43 +0800

    1 git hello

---
+++ src/com/atguigu/egit/test/HelloTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.egit.test;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git hello");
+	}
+
+}
